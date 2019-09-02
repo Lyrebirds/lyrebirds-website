0fe1e6180280043c35e4a3054442676bc9463acf
Abstrakten <kterndrupxd@gmail.com>
2019-09-02 16:25:15 +0200

    wrote about services, in preperation of i18n

---
+++ src/app/ourServices.ts
@@ -0,0 +1,23 @@
+export class Service {
+    title: string;
+    description: string;
+}
+
+export const OUR_SERVICES: Service[] = [
+    {
+        title: 'Red Team',
+        description: 'With this service we simulate a full scale attack'
+    },
+    {
+        title: 'Tech Checkup',
+        description: `IT-Security is constantly evolving with vulnerabilities being constantly discovered and corrected. Have you been keeping up with the latest news on the software your company uses, and whether or not it is still safe? We have, and we'll help you assess your preferred tools, so that your can safely leverage the technology.`
+    },
+    {
+        title: 'Employee Training',
+        description: `Modern attacks are not limited to technological exploitation, and defenses are often breached by assailants targeting well meaning employees. Your defenses are only as secure as the weakest point, and your employees are a major part of those defenses. We offer workshops on recognizing attacks, such as phishing or social engineering, tailored to your specific security strategy. Let us help your company by educating your staff on their part in keeping up IT-security, and transform your employees from liabilities to valuable security assets.`
+    },
+    {
+        title: 'Data security (GDPR)',
+        description: 'Also cookies'
+    },
+]
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OUR_SERVICES } from '../ourServices';
 
 @Component({
   selector: 'app-services',
@@ -7,24 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  serviceList = [
-    {
-      title: 'Red Team',
-      description: 'With this service we simulate a full scale attack'
-    },
-    {
-      title: 'Tech Checkup',
-      description: 'Often times, technology with known vulnerabilities are still in use, due to lack of awareness. Let us check it and update it, as this is quite easy and should be done. Often times, technology with known vulnerabilities are still in use, due to lack of awareness. Let us check it and update it, as this is quite easy and should be done.Often times, technology with known vulnerabilities are still in use, due to lack of awareness. Let us check it and update it, as this is quite easy and should be done. '
-    },
-    {
-      title: 'Employee training/testing',
-      description: 'With this service we simulate a full scale attack'
-    },
-    {
-      title: 'Data security (GDPR)',
-      description: 'With this service we simulate a full scale attack'
-    },
-  ]
+  serviceList = OUR_SERVICES
 
   columns = 2
 
