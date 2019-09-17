e3144a39cf0c6932f06f9fd7237440268f084db6
Abstrakten <kterndrupxd@gmail.com>
2019-09-17 14:05:40 +0200

    made about and graffiti hidden for production

--- src/app/employeeList.ts
+++ src/app/employeeList.ts
@@ -3,21 +3,21 @@ import { Employee } from './employee';
 export const EMPLOYEES: Employee[] = [
   {
     id: 0,
-    title: 'Lorem Ipsum',
+    title: 'Consultant',
     name: 'Kasper Kohsel Terndrup',
     email: 'kter@lyrebirds.dk',
     image: "/assets/img/default-avatar.jpg"
   },
   {
     id: 1,
-    title: 'Lorem Ipsum',
+    title: 'Consultant',
     name: 'Jens Hegner Stærmose',
     email: 'jhs@lyrebirds.dk',
     image: "/assets/img/default-avatar.jpg"
   },
   {
     id: 2,
-    title: 'Lorem Ipsum',
+    title: 'Consultant',
     name: 'Alexander Krog',
     email: 'akrog@lyrebirds.dk',
     image: "/assets/img/default-avatar.jpg"
