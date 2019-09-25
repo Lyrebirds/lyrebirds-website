5fde2d8e99f6f3d524a1d6115fe1aa35fececd3d
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2019-09-25 16:47:59 +0200

    Updated contact endpoint to request trello and have raid limit

--- flask-endpoint/index.py
+++ flask-endpoint/index.py
@@ -1,34 +1,34 @@
 from flask import Flask
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask import request
 import requests
 import os
 import json
+import time
 
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["100 per day", "10 per hour"]
+)
 
 trelloAppKey = os.environ.get('TRELLO_APP_KEY')
 trelloAppToken = os.environ.get('TRELLO_APP_TOKEN')
 contactBoardListId = "5d828870c72fb21602d25480"
 
-@app.route("/contact")
+@app.route("/contact", methods = ['POST'])
 def contact():
     name = request.args.get('name')
     email = request.args.get('email')
     phone = request.args.get('phone')
     subject = request.args.get('subject')
-    messageText = request.args.get('messageText')
+    messageText = request.args.get('body')
 
-    url = f'https://api.trello.com/1/cards?name={subject}&idList={contactBoardListId}&key={trelloAppKey}&token={trelloAppToken}'
-    
-    #querystring = {"id": "createCard", "name": subject, "idList": contactBoardListId}
+    url = f'https://api.trello.com/1/cards?name="{subject}" | {name} | {time.strftime("%Y-%m-%d %H:%M")}&desc={messageText}\n\n{name}\n{email}\n{phone}&idList={contactBoardListId}&key={trelloAppKey}&token={trelloAppToken}'
 
-    print('test')
-
-    response = requests.post(url)
-
-    return response.text
+    return requests.post(url).text
 
 if __name__ == "__main__":
     app.run()
-
-# Test string: http://localhost:5000/contact?name=Jens&email=jhs@lyrebirds.dk&phone=12345678&subject=Test&messageText=This%20is%20a%20test
