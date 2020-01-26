d958d698f76ba0a733e6e8ca6a1411e6aba11806
Alexander Krog <moonfrogdk@gmail.com>
2020-01-26 19:15:05 +0100

    about us updated and services got new look

--- src/app/employeeList.ts
+++ src/app/employeeList.ts
@@ -5,18 +5,18 @@ export const EMPLOYEES: Employee[] = [
     id: 0,
     name: 'Kasper Kohsel Terndrup',
     email: 'kter@lyrebirds.dk',
-    image: "/assets/img/kasper.jpg"
+    image: "/assets/img/kasper-cirkel.png"
   },
   {
     id: 1,
     name: 'Jens Hegner Stærmose',
     email: 'jhs@lyrebirds.dk',
-    image: "/assets/img/jens.jpg"
+    image: "/assets/img/jens-cirkel.png"
   },
   {
     id: 2,
     name: 'Alexander Dalsgaard Krog',
     email: 'akrog@lyrebirds.dk',
-    image: "/assets/img/alexander.jpg"
+    image: "/assets/img/alexander-cirkel.png"
   }
 ]
