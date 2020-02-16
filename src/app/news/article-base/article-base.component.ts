bbb5e674803ffb30ed8549f6351c224dda7deef6
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2020-02-16 17:03:27 +0100

    Articles now have translations

--- src/app/news/article-base/article-base.component.ts
+++ src/app/news/article-base/article-base.component.ts
@@ -9,9 +9,11 @@ export class ArticleBaseComponent {
   @Input() dedicated: boolean = true;
   route: string;
   id: string;
+  titleKey: string;
 
-  constructor(inputRoute: string, inputId: string) {
+  constructor(inputRoute: string, inputId: string, inputTitleKey: string) {
     this.route = inputRoute;
     this.id = inputId;
+    this.titleKey = inputTitleKey;
   }
 }
