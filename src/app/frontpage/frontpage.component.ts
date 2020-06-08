96741d6e498ee0d6d996329ac8352b9fa7345461
Jens Hegner Stærmose <jensstaermose@hotmail.com>
2020-06-08 17:09:36 +0200

    Style changes to spec

--- src/app/frontpage/frontpage.component.ts
+++ src/app/frontpage/frontpage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-frontpage',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrontpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) { }
+
+  @ViewChild("tabletImage") img: ElementRef;
+  @ViewChild("aboutCard") aboutCard: ElementRef;
 
   ngOnInit(): void {
+  }
+
+  update(): void {
+    this.aboutCard.nativeElement.ngAfterViewInit +=
+      console.log(this.img);
+    let boundingClientRect = this.aboutCard.nativeElement.getBoundingClientRect();
+    let bla = this.aboutCard.nativeElement;
+    console.log(bla.style);
+    let height = boundingClientRect.height;
+    console.log(height);
+    this.img.nativeElement.style.height = height + "px";
+    setTimeout(_ => {
+      this.update();
+    }, 100)
+  }
+
+  ngAfterViewInit(): void {
+    this.update();
   }
 
 }
